a2efd37da8d93128160d52695936ac59e00dbb77
s-beats <sakashin10291029@gmail.com>
2021-11-06 01:09:35 +0900

    hello cmd

--- go/main.go
+++ go/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 )
 
 func main() {
-	flag.Parse()
+	flag.CommandLine.Parse(os.Args[1:])
+	fmt.Println("Hello CLI")
 	fmt.Println(flag.Arg(0), flag.Arg(1))
 }
